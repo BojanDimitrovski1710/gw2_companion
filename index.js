e0257cecf662133749523e10c0375b221438956c
BojanDimitrovski1710 <70719546+BojanDimitrovski1710@users.noreply.github.com>
2021-05-05 23:57:44 +0200

    initial file dump

---
+++ index.js
@@ -0,0 +1,74 @@
+var nav_state = 0;
+
+function sleep(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+  }
+
+async function openNav(){
+    if(!nav_state){
+        $('.left').css('width' , '20%')
+        $('.right').css('width' , '80%')     
+        $('.nav_bar_content li').css('display' , 'block')
+        nav_state = 1;
+    }else{
+        $('.left').css('width' , '0%')
+        $('.right').css('width' , '100%')
+        $('.nav_bar_content li').css('display' , 'none')
+        nav_state = 0;
+       
+    }
+}
+
+const api_url = 'https://api.guildwars2.com/';
+
+async function check_raids(){
+    const raid_url = api_url + 'v2/account/raids?access_token=';
+    const raids = await fetch(raid_url + localStorage.getItem("api_key"));
+
+    var data = await raids.json(); 
+    
+    check_bosses(data);
+    
+}
+
+function check_bosses(data){
+    
+    data.forEach(raid_boss => {
+        $('#' + raid_boss).css('color', 'lime');
+    });
+}
+
+function submit_api(){
+    api_key = document.getElementById("api-key").value;
+    localStorage.setItem("api_key" , api_key)
+ 
+}
+
+function check_api(){
+    alert(localStorage.getItem("api_key"));
+}
+
+function CheckboxReadOnly() {
+    return true;
+ }
+
+function open_about(){
+    document.location.href = "./about.html";
+}
+
+function open_services(){
+    document.location.href = "./services.html";
+}
+
+function open_contact(){
+    document.location.href = "./contact.html"
+}
+
+function open_todo(){
+    document.location.href = "./todo.html";
+}
+
+function open_home(){
+    document.location.href = "./home.html";
+    $('#check_btn').css('visibility', 'hidden');
+}
